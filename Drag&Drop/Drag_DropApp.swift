cce0787579f8280acaf4b70c08f1b0de90ca8f4b
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-09-26 16:22:40 +0500

    Initial Commit

---
+++ Drag&Drop/Drag_DropApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_DropApp.swift
+//  Drag&Drop
+//
+//  Created by A'zamjon Abdumuxtorov on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Drag_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
